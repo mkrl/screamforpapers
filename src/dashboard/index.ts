d8ca302c8f4b06e5fa3dc6ba05d14b774379fccb
Mikhail Korolev <self@mkrl.xyz>
2023-12-09 17:39:42 +0100

    Force onboarding when no secrets are set

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,12 +1,17 @@
 import '../app.pcss'
 import Dashboard from "../components/Dashboard.svelte";
+import {storageSync} from "../storage";
 
 async function render() {
     const target = document.getElementById("app");
-    const forcedState =
+    let forcedState =
         window.location.search === ''
             ? undefined
             : window.location.search.slice(1) as DashboardForcedState;
+    const { token, repo } = await storageSync.get()
+    if (!token || !repo) {
+        forcedState = 'welcome'
+    }
     if (target) {
         new Dashboard({ target, props: { forcedState } });
     }
